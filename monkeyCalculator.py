f55bd8ef7a864d6568aa336f3d7f9075988413fb
Adam Hawkins <Adam.Hawkins@vu.com>
2022-02-14 12:44:33 -0600

    First Draft

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -1,13 +1,18 @@
 def calculateTime():
     
     # This first line is provided for you
-    monkey_one = input("Is the first monkey smiling?:  ")
+    monkey_one = input("Is the first monkey smiling?: ")
     monkey_two = input("Is the second monkey smiling?: ")
 
+    if monkey_one == monkey_two:
+        print("Uh oh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
+    
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateTime() and run > python monkeyCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculateTime()
+#calculateTime()
